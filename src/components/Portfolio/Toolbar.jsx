3235b55dc89e433c5488900f15903fc396663c6b
IlyaAlpatov <alpatov30@gmail.com>
2023-03-25 15:17:08 +0300

    добавлен компонент фильтров

--- src/components/Portfolio/Toolbar.jsx
+++ src/components/Portfolio/Toolbar.jsx
@@ -1,5 +1,27 @@
-const Toolbar = () => {
-  return;
+import Button from "@mui/material/Button";
+
+const Toolbar = ({ filters, selected, onSelectFilter }) => {
+  const handleClick = (e, selectFilter) => {
+    onSelectFilter(selectFilter);
+  };
+
+  const filterBtns = filters.map((filter, filterIdx) => {
+    const isActive = filter === selected ? true : false;
+    const isDisabled = isActive ? true : false;
+
+    return (
+      <Button
+        onClick={(e) => handleClick(e, filter)}
+        key={filterIdx}
+        color="secondary"
+        disabled={isDisabled}
+        variant="outlined"
+      >
+        {filter}
+      </Button>
+    );
+  });
+  return <div className="filters-container">{filterBtns}</div>;
 };
 
 export default Toolbar;
