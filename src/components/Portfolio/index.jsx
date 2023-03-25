90c18c5341f9a852bb517588a739e8bd2088391d
IlyaAlpatov <alpatov30@gmail.com>
2023-03-25 15:18:01 +0300

    добавлена передача пропсов фильтрам и смена фильтра

--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -1,12 +1,33 @@
 import { Component } from "react";
 import Toolbar from "./Toolbar";
 import ProjectList from "./ProjectList";
+import portfolioData from "../../store/portfolioData";
 
 class Portfolio extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filters: ["All", "Websites", "Flayers", "Business Cards"],
+      selected: "All",
+      portfolioData,
+    };
+  }
+
+  onSelectFilter = (selectFilter) => {
+    this.setState({
+      selected: selectFilter,
+    });
+  };
+
   render() {
     return (
-      <div>
-        <Toolbar />
+      <div className="portfolio">
+        <Toolbar
+          filters={this.state.filters}
+          selected={this.state.selected}
+          onSelectFilter={this.onSelectFilter}
+        />
+
         <ProjectList />
       </div>
     );
